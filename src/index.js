f827901c09448d4c11d4b82ab699810bbcadeb66
engAbdullahHaif <engabdullahhanif@gmail.com>
2022-06-11 10:53:14 +0430

    adding events, console was not working cause I had filter option in console, and was busy with it for almost 3o mints, Lol

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDom from "react-dom";
 
 const books = [
   {
+    id: 1, 
     img: "https://images-na.ssl-images-amazon.com/images/I/71l9pCV99FL._AC_UL906_SR906,600_.jpg",
     author: "Colleen Hoover",
     title: "It Ends with Us: A Novel (1)"
   }
   ,
   {
+    id: 2, 
     img: "https://images-na.ssl-images-amazon.com/images/I/71l9pCV99FL._AC_UL906_SR906,600_.jpg",
     author: "Colleen Hoover",
     title: "It Ends with Us: A Novel (1)"
@@ -17,22 +19,31 @@ const books = [
 
 
 function BookList() {
+  
   return(
     <section>
-     {books.map((book) => {
-       return <Book book={book}></Book>
-     })}
+    {books.map((book) => {
+      return <Book key={book.id} {...book}></Book>
+    })}
     </section>
     );
 }
 
+
 const Book = (props) => {
-  const {img, author, title} =  props.book;
+  const {img, author, title} =  props;
+  const clickHandler = () => {
+    console.log('I was triggered during componentDidMount')
+    // alert("Great Shot!");
+
+  };
+  
   return (
     <article>
       <img src={img} />
-      <h1>{author}</h1>
-      <h4>{title}</h4>
+      <h1 onClick={()=> {console.log('this is console')}} >{author}</h1>
+      <h4 >{title}</h4>
+      <button onClick={clickHandler}>click here</button>
     </article>
   );
 };
